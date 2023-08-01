73acd49620ecf2dc3496f76a01f723f37eb5c869
DaniSuarezG <danielsuarezgalvez@gmail.com>
2023-08-01 17:22:34 +0100

    .env enabled for Alter tables

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const addRelationsToModels = require('./database/relations')
 async function startDB() {
     await checkConnection()
     await addRelationsToModels()
-    await syncModels()
+    await syncModels(process.env.ALTER)
 
 }
 
